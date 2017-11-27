3c13539ce0da3b472ae357a9df4d4d3a209efb60
James Luo <jluo@bglcorp.com.au>
2017-11-27 13:14:54 -0800

    change the dim

--- keras_free.py
+++ keras_free.py
@@ -53,7 +53,7 @@ class PilotKeras:
         # This model currently assumes that the features of the model are just the images. Feel free to change this.
         steering_angle = float(self.model.predict(transformed_image_array, batch_size=1))
         # The driving model currently just outputs a constant throttle. Feel free to edit this.
-        throttle = 0.4
+        throttle = 0.6
         print(steering_angle, throttle)
 
         return steering_angle, throttle
